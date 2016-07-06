e34a1c871bec38e15fb58982b6f4f0aeb7d70f5b
Bree Cummins <breecummins@gmail.com>
2016-07-06 12:01:05 -0600

    removed debugging statements and uncommented code. Debugging apparently complete.

--- shellscripts/networkperturbations.sh
+++ shellscripts/networkperturbations.sh
@@ -15,10 +15,6 @@ HELPER_SCRIPT_CMD=$6
 QSUB=$7
 # QUERIES=$8 # QUERIES is an associative array of elements (key, query_cmd, query_arg, summary_cmd)
 
-echo $PATTERNDIR
-echo $QSUB
-printf "echoed QSUB"
-
 # for each perturbation, start a scheduled job for analysis
 for NETWORKFILE in $NETWORKDIR/*; do
 	# strip the uniquely identifying number off of the filename
