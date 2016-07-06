b82dc8a44164f7a7ecb50840ec4f8da3a8cb4a81
Bree Cummins <breecummins@gmail.com>
2016-07-06 16:03:05 -0600

    fixed bash variable name

--- shellscripts/querylibrary.sh
+++ shellscripts/querylibrary.sh
@@ -2,16 +2,16 @@ getnumparams() { dsgrn network $1 parameter | sed 's/[^0-9]*\([0-9]*\)[^0-9]*/\1
 getcountuniquelines() { cut -d " " -f 1 $1 | sort | uniq | wc -w; } # count unique parameters in file list (input is file path)
 getcountfromfile() { cat $1 | tr -d "\n"; } # pull count number out of file (input is file path)
 
-getstableFClist() { sqlite3 -separator " " $DATABASEFILE 'select ParameterIndex, Vertex from Signatures natural join (select MorseGraphIndex,Vertex from (select MorseGraphIndex,Vertex from MorseGraphAnnotations where Label="FC" except select MorseGraphIndex,Source from MorseGraphEdges));' > $DATABASEDIR/StableFCList$NETID.txt; } # dbname, outputfile
+getstableFClist() { sqlite3 -separator " " $DATABASEFILE 'select ParameterIndex, Vertex from Signatures natural join (select MorseGraphIndex,Vertex from (select MorseGraphIndex,Vertex from MorseGraphAnnotations where Label="FC" except select MorseGraphIndex,Source from MorseGraphEdges));' > $DATABASEDIR/StableFCList$NETWORKID.txt; } # dbname, outputfile
 
-summarystableFCs() { getcountuniquelines $DATABASEDIR/StableFCList$NETID.txt; } # do not delete file -- need for pattern matching
+summarystableFCs() { getcountuniquelines $DATABASEDIR/StableFCList$NETWORKID.txt; } # do not delete file -- need for pattern matching
 
-getmultistabilitylist() { sqlite3 -separator " " $DATABASEFILE 'select count(*) from Signatures natural join (select MorseGraphIndex from (select MorseGraphIndex, count(*) as numMinimal from (select MorseGraphIndex,Vertex from MorseGraphVertices except select MorseGraphIndex,Source from MorseGraphEdges) group by MorseGraphIndex) where numMinimal > 1);'  > $DATABASEDIR/Multistability$NETID.txt; } # dbname, outputfile
+getmultistabilitylist() { sqlite3 -separator " " $DATABASEFILE 'select count(*) from Signatures natural join (select MorseGraphIndex from (select MorseGraphIndex, count(*) as numMinimal from (select MorseGraphIndex,Vertex from MorseGraphVertices except select MorseGraphIndex,Source from MorseGraphEdges) group by MorseGraphIndex) where numMinimal > 1);'  > $DATABASEDIR/Multistability$NETWORKID.txt; } # dbname, outputfile
 
-summarymultistability() { getcountfromfile $DATABASEDIR/Multistability$NETID.txt; rm $DATABASEDIR/Multistability$NETID.txt; }
+summarymultistability() { getcountfromfile $DATABASEDIR/Multistability$NETWORKID.txt; rm $DATABASEDIR/Multistability$NETWORKID.txt; }
 
-makeFPquery() { $FPQUERY $DATABASEFILE $1 > $DATABASEDIR/FPresult$NETID.txt; } # FPQUERY DBNAME SPECIFIC_FP > OUTPUTFILE
+makeFPquery() { $FPQUERY $DATABASEFILE $1 > $DATABASEDIR/FPresult$NETWORKID.txt; } # FPQUERY DBNAME SPECIFIC_FP > OUTPUTFILE
 
-summaryFP() { getcountuniquelines $DATABASEDIR/FPresult$NETID.txt; rm $DATABASEDIR/FPresult$NETID.txt; }
+summaryFP() { getcountuniquelines $DATABASEDIR/FPresult$NETWORKID.txt; rm $DATABASEDIR/FPresult$NETWORKID.txt; }
 
 
