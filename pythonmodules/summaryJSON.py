52c031d2c0621da4201214b87fd1c38673b547b9
Bree Cummins <breecummins@gmail.com>
2016-08-02 16:24:15 -0600

    trying to avoid escaped quotes

--- pythonmodules/summaryJSON.py
+++ pythonmodules/summaryJSON.py
@@ -3,8 +3,15 @@ import sys,json
 network_spec_file=sys.argv[1]
 pattern_spec_file=sys.argv[2]
 results_file=sys.argv[3]
-results_dict = dict([tuple(pair.split(':', 1)) for pair in sys.argv[4].split() if pair])
 nummatches=sys.argv[5]
+
+results_dict = dict()
+
+for pair in sys.argv[4].split():
+    name, val = pair.split(':', 1)
+    try: val = eval(val)
+    except: pass
+    dict[name]=val
 
 if nummatches:
 	results_dict["StableFCMatchesParameterCount"] = int(nummatches)
