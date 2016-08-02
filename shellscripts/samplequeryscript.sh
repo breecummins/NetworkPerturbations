5248ac549f3533dc1c4453e277ec8336ecb7381f
Bree Cummins <breecummins@gmail.com>
2016-08-02 16:10:37 -0600

    unknown bug. trying to fix by swapping quote types

--- shellscripts/samplequeryscript.sh
+++ shellscripts/samplequeryscript.sh
@@ -2,7 +2,7 @@
 
 . shellscripts/querylibrary.sh # import custom functions
 
-FPINPUT="{'SBF':[2,8],'HCM1':[1,8],'NDD1':[0,0],'SWI5':[0,0],'YOX1':[0,0]}"
+FPINPUT='{"SBF":[2,8],"HCM1":[1,8],"NDD1":[0,0],"SWI5":[0,0],"YOX1":[0,0]}'
 python $DSGRN/software/FPQuery/FPQuery2.py $DATABASEFILE SingleFP $FPINPUT > $DATABASEDIR/query$NETWORKID.txt
 NUMFP=`getcountuniquelines $DATABASEDIR/query$NETWORKID.txt`
 rm $DATABASEDIR/query$NETWORKID.txt
