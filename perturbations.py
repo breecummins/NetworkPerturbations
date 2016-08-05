8fa88571efa481d1c115188b757b875a11619e96
Bree Cummins <breecummins@gmail.com>
2016-08-05 15:16:21 -0600

    made qsub an input argument

--- perturbations.py
+++ perturbations.py
@@ -1,4 +1,9 @@
 from pythonmodules.makejobs import Job 
+import sys
 
-job=Job(qsub=False)
-job.run()	
+if sys.argv[1] not in ['0','1']:
+    print "Input argument must be 1 (run on conley3) or 0 (run on hpc)."
+    sys.exit()
+else:
+    job=Job(int(sys.argv[1]))
+    job.run()	
