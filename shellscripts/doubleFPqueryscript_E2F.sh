4bbbcf38b6454a8bcba5390168df8c71597592a6
Bree Cummins <breecummins@gmail.com>
2016-09-14 09:53:31 -0600

    fixed shell script

--- shellscripts/doubleFPqueryscript_E2F.sh
+++ shellscripts/doubleFPqueryscript_E2F.sh
@@ -4,7 +4,7 @@
 
 FP1='{"E2F":[0,0],"E2F_Rb":[1,1]}' 
 FP2='{"E2F":[1,8],"E2F_Rb":[0,0]}'
-python $DSGRN/software/FPQuery/FPQuery2.py $DATABASEFILE DoubleFP $FP1 $FP2 > $DATABASEDIR/bistability_net4_$NETWORKID.txt
+python $DSGRN/software/FPQuery/FPQuery2.py $DATABASEFILE DoubleFP $FP1 $FP2 > $DATABASEDIR/query$NETWORKID.txt
 NUMFP=`getcountuniquelines $DATABASEDIR/query$NETWORKID.txt`
 rm $DATABASEDIR/query$NETWORKID.txt
 echo "DoubleFPQuery:$FP1 $FP2 ** DoubleFPQueryParameterCount:"$NUMFP #split entries by **
--- shellscripts/doubleFPqueryscript_E2F_withparamfiles.sh
+++ shellscripts/doubleFPqueryscript_E2F_withparamfiles.sh
@@ -4,6 +4,6 @@
 
 FP1='{"E2F":[0,0],"E2F_Rb":[1,1]}' 
 FP2='{"E2F":[1,8],"E2F_Rb":[0,0]}'
-python $DSGRN/software/FPQuery/FPQuery2.py $DATABASEFILE DoubleFP $FP1 $FP2 > $DATABASEDIR/query$NETWORKID.txt
+python $DSGRN/software/FPQuery/FPQuery2.py $DATABASEFILE DoubleFP $FP1 $FP2 > $DATABASEDIR/bistability_net4_$NETWORKID.txt
 NUMFP=`getcountuniquelines $DATABASEDIR/query$NETWORKID.txt`
-echo  $SC" ** DoubleFPQuery:$FP1 $FP2 ** DoubleFPQueryParameterCount:"$NUMFP #split entries by **
+echo  " ** DoubleFPQuery:$FP1 $FP2 ** DoubleFPQueryParameterCount:"$NUMFP #split entries by **
