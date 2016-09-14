8ba7c57feb37f1f92bed81991d847f7c2f3d22a4
Bree Cummins <breecummins@gmail.com>
2016-09-14 09:48:25 -0600

    added new query script

---
+++ shellscripts/doubleFPqueryscript_E2F_withparamfiles.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+. shellscripts/querylibrary.sh # import custom functions
+
+FP1='{"E2F":[0,0],"E2F_Rb":[1,1]}' 
+FP2='{"E2F":[1,8],"E2F_Rb":[0,0]}'
+python $DSGRN/software/FPQuery/FPQuery2.py $DATABASEFILE DoubleFP $FP1 $FP2 > $DATABASEDIR/query$NETWORKID.txt
+NUMFP=`getcountuniquelines $DATABASEDIR/query$NETWORKID.txt`
+echo  $SC" ** DoubleFPQuery:$FP1 $FP2 ** DoubleFPQueryParameterCount:"$NUMFP #split entries by **
