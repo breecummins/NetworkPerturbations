dd58f5950a883cb06792e73a7a474504b545e98e
Bree Cummins <breecummins@gmail.com>
2016-09-30 15:49:52 -0600

    wrote full inducibility for E2F networks

--- analysis/fullinducibility.py
+++ analysis/fullinducibility.py
@@ -59,6 +59,45 @@ def fullinducibilityquery_Yao(databasefolder='/Users/bcummins/ProjectSimulationR
     with open('/Users/bcummins/ProjectSimulationResults/YaoNetworks/YaoNetworks_nonessential_fullinducibilityresults.json','w') as f:
         json.dump(fullInducDict,f)
 
+def fullinducibilityquery_E2F(databasefolder='/Users/bcummins/ProjectSimulationResults/E2FNaturePaper'):
+
+    FP_OFF='{"E2F":[0,0],"E2F_Rb":[1,1]}' 
+    FP_ON='{"E2F":[1,8],"E2F_Rb":[0,0]}'
+
+    fullInducDict = {}
+
+    for db in os.listdir(databasefolder):
+        if db[-2:] == 'db':
+            database = qDSGRN.dsgrnDatabase(os.path.join(databasefolder,db))
+            size_factor_graph,num_factor_graphs = database.single_gene_query_prepare("S")
+            print(database.network.specification())
+            print str(num_factor_graphs) + "\n"
+            matchesBiStab = frozenset(database.DoubleFPQuery(FP_ON,FP_OFF))
+            matchesOFF = frozenset(set(database.SingleFPQuery(FP_OFF)).difference(matchesBiStab))
+            matchesON = frozenset(set(database.SingleFPQuery(FP_ON)).difference(matchesBiStab))
+            min_gpi = 0
+            max_gpi = size_factor_graph-1
+            OFF = set([])
+            ON = set([])
+            BiStab = set([])
+            for m in range(num_factor_graphs):
+                graph = database.single_gene_query("S", m)
+                if graph.mgi(min_gpi) in matchesOFF:
+                    OFF.add(m)
+                if graph.mgi(max_gpi) in matchesON:
+                    ON.add(m)
+                for i in range(min_gpi+1,max_gpi):
+                    if graph.mgi(i) in matchesBiStab:
+                        BiStab.add(m)
+                        break
+            resettablebistability = BiStab.intersection(OFF)
+            inducibility = BiStab.intersection(ON)
+            fullinducibility = len(resettablebistability.intersection(inducibility))
+            fullInducDict[database.network.specification()] = (len(BiStab),len(resettablebistability),len(inducibility),fullinducibility,num_factor_graphs)
+    print fullInducDict
+    with open('/Users/bcummins/ProjectSimulationResults/6D_2016_08_26_cancerE2Fnetwork1_fullinducibilityresults.json','w') as f:
+        json.dump(fullInducDict,f)
+
 if __name__ == "__main__":
     # makeYaoDatabases()
     # makeYaoDatabases('/Users/bcummins/ProjectSimulationResults/YaoNetworks/4D_2016_08_25_Yao.json')
