6ba690904dc8d24ec34082dc4d31984b9b8a410d
Bree Cummins <breecummins@gmail.com>
2018-02-09 12:34:24 -0700

    Working merge tree algorithm.

--- queries/patternmatch/triplet_merge_trees.py
+++ queries/patternmatch/triplet_merge_trees.py
@@ -1,63 +1,64 @@
 # reference: Dmitriy Smirnov and Dmitriy Morozov Chapter 1 Triplet Merge Trees
 # people.csail.mit.edu/smirnov/publications/tmt.pdf
+# Algorithms 1 & 2
 
-
-import operator
-
-#FIXME: Double-check that this actually works for maxima
-
-def ComputeMergeTree(curve, type="min"):
+def ComputeMergeTree(curve):
     '''
     :param curve: dict with times keying function values
-    :param type: "min" or "max"
-    :return: triplet merge tree representation, a dict with a time keying a tuple of times, T[u] = (s,v)
+    :return: triplet merge tree representation --
+             a dict with a time keying a tuple of times, T[u] = (s,v),
+             where u obtains label v at time s (branch decomposition)
 
     '''
 
-    if type == "min":
-        op = operator.lt
-        ope = operator.le
-    else:
-        op = operator.gt
-        ope = opertor.ge
-
-    def Merge(T, u, s, v):
-        (u1, su, u2) = Representative(T, u, s)
-        (v1, sv, v2) = Representative(T, v, s)
-        if u1 == v1: # u1 == v2? possible typo?
-            return T
-        if op(curve[v1], curve[u1]):
-            (u1, su, u2), (v1, sv, v2) = (v1, sv, v2), (u1, su, u2)
-        T[v1] = (s, u1)
-        return Merge(T, u1, sv, v2)
-
-    def Representative(T, u, s):
-        a = curve[s]
-        (s,v) = T[u]
-        while s != v and ope(curve[s], a):
-            u = v
-            (s,v) = T[u]
-        return (u,s,v)
+    def finddeepest(u):
+        u1 = deepest[u]
+        (_,v) = T[u1]
+        while u1 != v:
+            u1 = deepest[v]
+            (_,v) = T[u1]
+        d = u1
+        u1 = deepest[u]
+        (_, v) = T[u1]
+        while u1 != v:
+            u1 = deepest[v]
+            deepest[v] = d
+            (_, v) = T[u1]
+        deepest[u] = d
+        return d
 
     T = dict( (t,(t,t)) for t in curve )
+    deepest = dict( (t,t) for t in curve )
     times = sorted([t for t in curve])
-    for k in range(len(times[:-1])):
-        u,v = times[k],times[k+1]
-        if op(curve[u],curve[v]):
-            T = Merge(T,v,v,u)
-        else:
-            T = Merge(T,u,u,v)
-    for u in T:
-        # Repair algorithm from paper
-        (s,_) = T[u]
-        (_,_,v) = Representative(T,u,s)
-        if u != v: T[u] = (s,v)
+    edges = zip(times[:-1],times[1:])
+    sorted_verts = [a for (a,b) in sorted(curve.iteritems(), key=lambda (k,v): v)]
+    for u in sorted_verts:
+        leaves = []
+        for e in edges:
+            if u in e:
+                v = e[0] if u != e[0] else e[1]
+                if curve[v] < curve[u]:
+                    leaves.append(finddeepest(v))
+        if len(leaves) > 0:
+            inds = sorted(zip([sorted_verts.index(l) for l in leaves],leaves))
+            w = min(inds)[1]
+            T[u] = (u,w)
+            leaves.remove(w)
+            for x in leaves:
+                T[x] = (u,w)
     return T
+
+
+
+
+
+
+
 
 def test():
     curve = {0:-1, 1:2, 2:0, 3:3, 4:-2, 5:1, 6:-3}
     tmt = {0:(3,6),1:(1,0),2:(1,0),3:(3,6),4:(5,6),5:(5,6),6:(6,6)}
-    T = ComputeMergeTree(curve)
+    T = ComputeMergeTree2(curve)
     print(tmt)
     print(T)
 
