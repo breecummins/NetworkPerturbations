4b22fd2052a17ac01cd6a0a6da8cd5401f7112c2
Bree Cummins <breecummins@gmail.com>
2018-11-20 11:50:48 -0700

    moved tests

--- setup.py
+++ setup.py
@@ -3,8 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='NetworkPerturbations',
     version='0.0.1',
-    packages=find_packages(),
-    package_dir = {'': 'libnetperturb'},
+    packages=find_packages(exclude=["tests","examples",]),
     install_requires=[],
     author="Bree Cummins",
     url='https://github.com/breecummins/NetworkPerturbations'
