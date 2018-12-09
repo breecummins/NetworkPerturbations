946477f21d6c955d0a32cdbc62b8b618e7c4df9d
Bree Cummins <breecummins@gmail.com>
2018-12-09 12:00:22 -0700

    going to analyze more networks. Original list did not have any successes for the design parameter. In particular, the original network was not recovered.

--- scripts/voigtdesign.py
+++ scripts/voigtdesign.py
@@ -3,12 +3,11 @@ import NetworkPerturbations.perturbations.fileparsers as fp
 import NetworkPerturbations.perturbations.graphtranslation as gt
 import NetworkPerturbations.queries.MultistabilityExists as ME
 import networkx as nx
-import random
-from pprint import pprint
+import json
 
 
 def run():
-    network_spec = "A : A : E\nB : B : E\nC : C : E\nx : A : E\ny : B : E\nz : C : E\nYFP : : E\nD : D + YFP : E"
+    network_spec = "C : C : E\nB : B : E\nA : A : E\nx : A : E\ny : B : E\nz : C : E\nYFP : : E\nD : D + YFP : E"
 
     params = {
         "edgelist" : fp.parseEdgeFile("VoigtEdgeFileShort.txt"),
@@ -53,13 +52,10 @@ def run():
         if all(len(s) == 1 for s in scc):
             filtered_networks.append(ns)
     print("Number of filtered networks: {}".format(len(filtered_networks)))
-    # for _ in range(10):
-    #     print("\n")
-    #     pprint(random.choice(filtered_networks))
-
+    print("Saving feed-forward networks.")
+    json.dump(filtered_networks,open("temp/all_networks_tested.json"))
     print("Searching for truth tables...")
-    # ME.query(networks[1:],"temp",query_params)
-    ME.query(networks[1:],"temp",query_params)
+    ME.query(filtered_networks,"temp",query_params)
 
 if __name__ == "__main__":
     run()
