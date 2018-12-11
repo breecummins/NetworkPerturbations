b3dd180b704e4366455543be1e1e27208a6235c5
Bree Cummins <breecummins@gmail.com>
2018-12-10 18:05:47 -0700

    implemented feed-forward filter

--- lib/NetworkPerturbations/perturbations/filters.py
+++ lib/NetworkPerturbations/perturbations/filters.py
@@ -1,7 +1,23 @@
+import networkx as nx
+
+
 def constrained_inedges(graph,kwargs):
-    message = "Number of in-edges not in range."
+    # kwargs = { "min_inedges" : integer, "max_inedges" : integer }
+    # return bool (True if satisfied) and string containing error message
     for u in graph.vertices():
         N = len([v for v in graph.vertices() if u in graph.adjacencies(v)])
         if N < kwargs["min_inedges"] or N > kwargs["max_inedges"]:
-            return False, message
+            return False, "Number of in-edges not in range."
+    return True, ""
+
+
+def is_feed_forward(graph,kwargs):
+    # kwargs = {}, here only for API compliance
+    # return bool (True if satisfied) and string containing error message
+    G = nx.DiGraph()
+    G.add_edges_from(graph.edges())
+    scc = nx.strongly_connected_components(G)
+    # throw out graphs with non-trivial cycles
+    if any(len(s) > 1 for s in scc):
+        return False, "Contains cycles."
     return True, ""
--- scripts/voigtdesign.py
+++ scripts/voigtdesign.py
@@ -1,8 +1,6 @@
 import NetworkPerturbations.perturbations.networkperturbations as netper
 import NetworkPerturbations.perturbations.fileparsers as fp
-import NetworkPerturbations.perturbations.graphtranslation as gt
 import NetworkPerturbations.queries.MultistabilityExists as ME
-import networkx as nx
 import json
 
 
@@ -15,9 +13,9 @@ def run():
         "probabilities" : {"addNode" : 0.5, "removeNode" : 0.00, "addEdge" : 0.5, "removeEdge" : 0.00},
         "range_operations" : [4,16],
         "numperturbations" : 10000,
-        "time_to_wait" : 600,
+        "time_to_wait" : 7200,
         "maxparams" : 10000,
-        "filters" : [{"constrained_inedges" : {"min_inedges" : 1, "max_inedges" : 2}}]
+        "filters" : [{"constrained_inedges" : {"min_inedges" : 1, "max_inedges" : 2}}, {"is_feed_forward" : {}}]
     }
 
     query_params = {
@@ -40,20 +38,10 @@ def run():
     }
 
     networks = netper.perturbNetwork(params,network_spec)
-    filtered_networks = []
-    for ns in networks:
-        graph = gt.getGraphFromNetworkSpec(ns)
-        G = nx.DiGraph()
-        G.add_edges_from(graph.edges())
-        scc = nx.strongly_connected_components(G)
-        # throw out graphs with non-trivial cycles
-        if all(len(s) == 1 for s in scc):
-            filtered_networks.append(ns)
-    print("Number of filtered networks: {}".format(len(filtered_networks)))
     print("Saving feed-forward networks.")
-    json.dump(filtered_networks,open("temp/all_networks_tested.json","w"))
-    print("Searching for truth tables...")
-    ME.query(filtered_networks,"temp",query_params)
+    json.dump(networks,open("temp/all_networks_tested.json","w"))
+    # print("Searching for truth tables...")
+    # ME.query(networks,"temp",query_params)
 
 if __name__ == "__main__":
     run()
