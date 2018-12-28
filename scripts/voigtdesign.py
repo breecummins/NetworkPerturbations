42cf5f6585dad58537fad27392fd5066ed85c289
Bree Cummins <breecummins@gmail.com>
2018-12-28 14:43:06 -0700

    new output folder in voigt script

--- scripts/voigtdesign.py
+++ scripts/voigtdesign.py
@@ -1,7 +1,7 @@
 import NetworkPerturbations.perturbations.networkperturbations as netper
 import NetworkPerturbations.perturbations.fileparsers as fp
 import NetworkPerturbations.queries.MultistabilityExists as ME
-import json
+import json, subprocess, sys, os
 
 
 def run():
@@ -12,9 +12,9 @@ def run():
         "nodelist" : fp.parseNodeFile("VoigtNodeFile.txt"),
         "probabilities" : {"addNode" : 0.8, "removeNode" : 0.00, "addEdge" : 0.2, "removeEdge" : 0.00},
         "range_operations" : [6,6],
-        "numperturbations" : 5,
+        "numperturbations" : 100000,
         "time_to_wait" : 7200,
-        "maxparams" : 5000,
+        "maxparams" : 6000,
         "filters" : {"constrained_outedges" : {"min_outedges" : 1, "max_outedges" : 3},
                      "constrained_inedges" : {"min_inedges" : 1, "max_inedges" : 2}, "is_feed_forward" : {},
                      }
@@ -40,18 +40,22 @@ def run():
         "hex_constraints" : {(1,1) : ["2"], (1,2) : ["C"], (1,3) : ["38"], (2,1) : ["E"], (2,2) : ["FC"], (2,3) : ["FF8"]}
     }
 
+    datetime = subprocess.check_output(['date +%Y_%m_%d_%H_%M_%S'], shell=True).decode(sys.stdout.encoding).strip()
+    outdir = "computations_"+datetime
+    os.makedirs(outdir)
+
     def perturb():
         networks = netper.perturbNetwork(params,network_spec)
         print("Saving feed-forward networks.")
-        json.dump(networks,open("temp/all_networks_tested.json","w"))
-        # print("Searching for truth tables...")
-        ME.query(networks,"temp",query_params)
+        json.dump(networks,open(os.path.join(outdir,"all_networks_tested.json"),"w"))
+        print("Searching for truth tables...")
+        ME.query(networks,outdir,query_params)
 
     def check_original():
         original = ["C : C : E\nB : B : E\nA : A : E\nw : C : E\nz: A + C : E\ny : B : E\nx : A : E\nt : (~y)(~x) : E\nu : "
                  "~t : E\nv : (~w)(~u) : E\nYFP : (~v)(~z) : E\nD : YFP + D : E"]
         ME.query(original, "temp", query_params)
-        net = json.load(open("temp/Networks_With_Multistable_FP.json"))
+        net = json.load(open(os.path.join(outdir,"Networks_With_Multistable_FP.json")))
         if len(net) == 1:
             print("\nOriginal network passes FP search.")
 
