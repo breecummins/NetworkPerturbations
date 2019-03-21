904f74164ba236501e3d2479c9864433fee1b784
Bree Cummins <breecummins@gmail.com>
2019-03-21 14:16:00 -0600

    moved project scripts to a different repository

--- scripts/slurmvoigt.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#SBATCH -p gpu               # partition (queue)
-#SBATCH -n 1                # number cores
-#SBATCH -N 1                 # Minimum one node
-#SBATCH -t 0-10:00           # Runtime in D-HH:MM
-
-time python voigtdesign.py
